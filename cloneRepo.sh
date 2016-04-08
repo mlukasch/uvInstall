a590d32b609672781033fe6338f94e864dbb1bab
Maik Lukasch <mlukasch@gmail.com>
2016-04-08 13:07:12 +0200

    adapted

--- cloneRepo.sh
+++ cloneRepo.sh
@@ -1,1 +1,1 @@
-mkdir -p app_volume/repo && cd app_volume/repo && git clone https://github.com/UnifiedViews/Core.git && git clone https://github.com/UnifiedViews/Plugin-DevEnv.git
+mkdir -p app_volume/repo && cd app_volume/repo && git clone https://github.com/UnifiedViews/Core.git && git clone https://github.com/UnifiedViews/Plugin-DevEnv.git && mkdir -p app_volume/repo/Core/target/dpu
--- createVolumeFolders.sh
+++ createVolumeFolders.sh
@@ -2,5 +2,4 @@ mkdir -p app_volume/data/backend/working
 mkdir -p app_volume/data/frontend/working
 mkdir -p app_volume/logs
 mkdir -p app_volume/logs/tomcat
-mkdir -p app_volume/repo/Core/target/dpu
 mkdir -p db_volume/data/psql
