36bd8ba7d15219bceb1a2a7fe33b4d0d475bfb2f
Maik Lukasch <mlukasch@gmail.com>
2016-04-08 11:19:33 +0200

    added

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sh ./createVolumeFolders.sh
 sh ./cloneRepo.sh
 
 sh ./createDbImage.sh
-#sh ./runDbContainer.sh
-
 sh ./createAppImage.sh
-#sh ./runAppContainer.sh
+
+sh ./runDbContainer.sh
+sh ./runAppContainer.sh
