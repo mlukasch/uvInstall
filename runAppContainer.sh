5abf30e8e5d3fdf43fbdec2172f5917bebe03d0d
Maik Lukasch <mlukasch@gmail.com>
2016-04-08 13:12:04 +0200

    adapted

--- runAppContainer.sh
+++ runAppContainer.sh
@@ -6,8 +6,7 @@ docker run \
    -v $(pwd)/app_volume:/volume \
    --link uv_db \
    --name uv_app \
-   -p 28080:28080 \
-   -p 25010:5010 \
    -p 28080:8080 \
+   -p 25010:5010 \
    -d \
    uv_app_img
